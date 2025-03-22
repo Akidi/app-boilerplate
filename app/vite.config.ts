e090727df10fda00f4073d2a7e5f9905df97e5d9
James Rhamy <1060896+Akidi@users.noreply.github.com>
2025-03-22 17:49:39 -0500

    chore: update project configuration and dependencies

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,33 +1,49 @@
-import { svelteTesting } from "@testing-library/svelte/vite";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
+import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
+const dirname =
+	typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineConfig({
-    plugins: [tailwindcss(), sveltekit()],
-
-    test: {
-        workspace: [{
-            extends: "./vite.config.ts",
-            plugins: [svelteTesting()],
-
-            test: {
-                name: "client",
-                environment: "jsdom",
-                clearMocks: true,
-                include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-                exclude: ['src/lib/server/**'],
-                setupFiles: ['./vitest-setup-client.ts']
-            }
-        }, {
-            extends: "./vite.config.ts",
-
-            test: {
-                name: "server",
-                environment: "node",
-                include: ['src/**/*.{test,spec}.{js,ts}'],
-                exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-            }
-        }]
-    }
+	server: {
+		watch: {
+			usePolling: true,
+			interval: 1000,
+			ignored: ['node_modules', 'dist', 'build', 'public', 'static']
+		},
+		host: true,
+		strictPort: true,
+	},
+	plugins: [tailwindcss(), sveltekit()],
+	test: {
+		workspace: [
+			{
+				extends: './vite.config.ts',
+				plugins: [svelteTesting()],
+				test: {
+					name: 'client',
+					globals: true,
+					environment: 'jsdom',
+					clearMocks: true,
+					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					exclude: ['src/lib/server/**'],
+					setupFiles: ['./vitest-setup-client.ts']
+				}
+			},
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.server.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}
+		]
+	}
 });
